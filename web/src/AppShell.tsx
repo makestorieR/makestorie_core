ef87487c64bf46601431c30f966329854ca7ec88
John <johnnonso090@gmail.com>
2023-05-09 11:40:59 +0100

    first setups

---
+++ web/src/AppShell.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppShell() {
+  return (
+    <div className="App">
+     Hey we are ready for tremendous development.
+    </div>
+  );
+}
+
+export default AppShell;
